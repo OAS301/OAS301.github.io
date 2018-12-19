06d13ebcaa5ef4e2ede25c2ea9e3b0cf27cacaa9
OAS301 <43147268+OAS301@users.noreply.github.com>
2018-12-19 14:36:59 +0300

    Update auth.js

--- lab2/js/auth.js
+++ lab2/js/auth.js
@@ -1,19 +1,3 @@
-(function($) {
-    
-    var SHOW_CLASS = 'show',
-        HIDE_CLASS = 'hide',
-        ACTIVE_CLASS = 'active';
-    $('.tabs').on('click', 'li a', function(e) {
-        e.preventDefault();
-        var $tab = $(this),
-            href = $tab.attr('href');
-        $('.active').removeClass(ACTIVE_CLASS);
-        $tab.addClass(ACTIVE_CLASS);
-        $('.show').removeClass(SHOW_CLASS).addClass(HIDE_CLASS).hide();
-        $(href).removeClass(HIDE_CLASS).addClass(SHOW_CLASS).hide().fadeIn(620);
-    });
-})(jQuery);
-
 validator.message['date'] = 'not a real date';
 
 $('form').on('blur', 'input[required], input.optional, select.required', validator.checkField).on('change', 'select.required', validator.checkField).on('keypress', 'input[required][pattern]', validator.keypress);
